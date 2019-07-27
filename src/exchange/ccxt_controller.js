adac64ce3ab535112afc9bc7820ac856abbf4096
Valamidev <Valamidev@users.noreply.github.com>
2019-07-27 13:25:23 +0200

    Style refactor

--- src/exchange/ccxt_controller.js
+++ src/exchange/ccxt_controller.js
@@ -1,55 +1,49 @@
-"use strict";
+"use strict"
 
-const logger = require("../logger");
-const _ = require("lodash");
+const logger = require("../logger")
+const _ = require("lodash")
 // CCXT
-const ccxt = require("ccxt");
+const ccxt = require("ccxt")
 
 class ExchangeAPI {
   constructor() {
-    this.exchanges = [];
+    this.exchanges = []
   }
 
-  async get_candlestick(
-    symbol,
-    exchange,
-    interval,
-    since = undefined,
-    limit = 100
-  ) {
+  async get_candlestick(symbol, exchange, interval, since = undefined, limit = 100) {
     try {
-      let API = this.load_exchange_api(exchange);
+      let API = this.load_exchange_api(exchange)
 
       /* [[time,open,high,close,volume]] */
-      let candledata = await API.fetchOHLCV(symbol, interval, since, limit);
+      let candledata = await API.fetchOHLCV(symbol, interval, since, limit)
 
-      return candledata;
+      return candledata
     } catch (e) {
-      logger.error("CCXT candlestick error ", e);
+      logger.error("CCXT candlestick error ", e)
     }
   }
 
   async get_marketdata(exchange) {
     try {
-      let API = this.load_exchange_api(exchange);
+      let API = this.load_exchange_api(exchange)
 
-      let marketdata = await API.loadMarkets();
+      let marketdata = await API.loadMarkets()
 
-      return marketdata;
+      return marketdata
     } catch (e) {
-      logger.error("CCXT marketdata error ", e);
+      logger.error("CCXT marketdata error ", e)
     }
   }
 
   async get_pricetickers(exchange) {
     try {
-      let API = this.load_exchange_api(exchange);
+      let API = this.load_exchange_api(exchange)
 
-      let pricetickers = await API.fetchTickers();
+      let pricetickers = await API.fetchTickers()
 
-      return pricetickers;
+      return pricetickers
     } catch (e) {
-      logger.error("CCXT marketdata error ", e);
+      logger.error("CCXT marketdata error ", e)
     }
   }
 
@@ -57,40 +51,40 @@ class ExchangeAPI {
 
   load_exchange_api(exchange) {
     try {
-      exchange = exchange.toLowerCase();
+      exchange = exchange.toLowerCase()
 
       // Check if CCXT API already loaded
-      let exchange_data = this.exchanges.filter(e => e.exchange == exchange);
+      let exchange_data = this.exchanges.filter((e) => e.exchange == exchange)
 
       // CCTX API load from buffer or add to the buffer
       if (exchange_data.length == 0) {
-        exchange_data = this.init_new_exchanges(exchange);
+        exchange_data = this.init_new_exchanges(exchange)
       } else {
-        exchange_data = exchange_data[0];
+        exchange_data = exchange_data[0]
       }
 
-      return exchange_data.api;
+      return exchange_data.api
     } catch (e) {
-      logger.error("CCXT load API error ", e);
+      logger.error("CCXT load API error ", e)
     }
   }
 
   init_new_exchanges(exchange) {
-    exchange = exchange.toLowerCase();
+    exchange = exchange.toLowerCase()
 
     // Check exchange is valid
     if (_.isObject(ccxt[exchange])) {
-      let api = new ccxt[exchange]();
+      let api = new ccxt[exchange]()
 
-      this.exchanges.push({ exchange, api });
+      this.exchanges.push({ exchange, api })
 
-      return { exchange, api };
+      return { exchange, api }
     } else {
-      throw `Invalid Exchange ${exchange}`;
+      throw `Invalid Exchange ${exchange}`
     }
   }
 
   /* CCXT API STUFF */
 }
 
-module.exports = new ExchangeAPI();
+module.exports = new ExchangeAPI()
