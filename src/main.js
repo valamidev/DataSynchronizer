8bdf94c7cc4e127d66cf8ea3d4e8992fd36a0460
Valamidev <Valamidev@users.noreply.github.com>
2019-08-01 19:50:34 +0200

    Redis is not optional anymore

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 const logger = require("./logger")
 require("./emitter") // Eventemitter class
+require("./redis")
 const SentimentAPI = require("./sentiment/sentiment")
 const LivefeedAPI = require("./livefeed/livefeed")
 const MarketDataAPI = require("./marketdata")
@@ -9,7 +10,7 @@ const PriceTickersAPI = require("./pricetickers")
 const WardenClass = require("./warden")
 
 // Load Dotenv variables
-const { MarketData, Sentiment, Livefeed, Redis, PriceTicker, Warden, exchange_list, warden_exchanges_list, quotes, quote_limits } = process.env
+const { MarketData, Sentiment, Livefeed, PriceTicker, Warden, exchange_list, warden_exchanges_list, quotes, quote_limits } = process.env
 
 const exchanges = exchange_list.split(",")
 const warden_exchanges = warden_exchanges_list.split(",")
@@ -30,10 +31,6 @@ async function main() {
     // Websocket support check exchanges/ws_exchanges for support!
     if (Livefeed == 1) {
       await LivefeedAPI.start(exchanges)
-    }
-    if (Redis == 1) {
-      // Redis Pub/Sub
-      require("./redis")
     }
     // Warden Auto init tradepairs for data collection based on Volume desc
     if (Warden == 1) await WardenClass.start(warden_exchanges, warden_quotes, warden_quote_limits)
