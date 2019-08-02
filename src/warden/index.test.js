5718cb3aacc3182c882b18f16a5b0a8f2561bdb3
Valamidev <Valamidev@users.noreply.github.com>
2019-08-02 17:44:51 +0200

    Update Warden test

--- src/warden/index.test.js
+++ src/warden/index.test.js
@@ -1,16 +1,16 @@
-"use strict";
-require("dotenv").config();
+"use strict"
+require("dotenv").config()
 
-const Warden = require("./index");
+const Warden = require("./index")
 
 test("Warden Select Symbols", async () => {
-  let result = await Warden.select_symbols("kucoin", "BTC", 10);
+  let result = await Warden.select_symbols("kucoin", "BTC", 1)
 
-  expect(result).toBeDefined();
-});
+  expect(result).toBeDefined()
+})
 
 test("Warden Start", async () => {
-  let result = await Warden.start(["kucoin", "binance"], ["BTC"], [10]);
+  let result = await Warden.start(["kucoin", "binance"], ["BTC"], [1])
 
-  expect(result).toBeUndefined();
-});
+  expect(result).toBeUndefined()
+})
