079496ef34bf169a421e524c446221c8a42ece5d
Valamidev <Valamidev@users.noreply.github.com>
2019-08-15 09:34:40 +0200

    Reddit API warning disable by default

--- src/sentiment/reddit/reddit_api.js
+++ src/sentiment/reddit/reddit_api.js
@@ -1,7 +1,7 @@
-"use strict";
+"use strict"
 
-const logger = require("../../logger");
-const snoowrap = require("snoowrap");
+const logger = require("../../logger")
+const snoowrap = require("snoowrap")
 
 const reddit_api = new snoowrap({
   userAgent: "SentimentBoT",
@@ -9,22 +9,22 @@ const reddit_api = new snoowrap({
   clientSecret: process.env.reddit_clientSecret,
   username: process.env.reddit_username,
   password: process.env.reddit_password
-});
+})
 
 class RedditAPI {
   constructor() {}
 
   async get_subreddit_top(subreddit) {
     try {
-      let posts = await reddit_api
-        .getSubreddit(subreddit)
-        .getTop({ time: "day" });
+      reddit_api.config({ requestDelay: 1000, warnings: false })
 
-      return posts;
+      let posts = await reddit_api.getSubreddit(subreddit).getTop({ time: "day" })
+
+      return posts
     } catch (e) {
-      logger.error("Reddit API ", e);
+      logger.error("Reddit API ", e)
     }
   }
 }
 
-module.exports = new RedditAPI();
+module.exports = new RedditAPI()
