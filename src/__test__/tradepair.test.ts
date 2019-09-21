5a410e6838244a750b5a090a7cd6c41f60eaff6e
Valamidev <Valamidev@users.noreply.github.com>
2019-09-21 13:54:18 +0200

    Tradepair test fix

--- src/__test__/tradepair.test.ts
+++ src/__test__/tradepair.test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 "use strict"
 require("dotenv").config()
+process.env.log_level = "info"
 
 import {TradepairQueries} from '../tradepairs/tradepairs'
 
@@ -9,11 +10,11 @@ test("Get BCHABC-ETH symbol", async () => {
   let symbol = await TradepairQueries.id_to_symbol("kucoin", "BCHABC-ETH")
 
   
- /* let symbol_cache = ""
+  let symbol_cache = ""
 
   for (let i = 0; i < 10; i++) {
     symbol_cache = await TradepairQueries.id_to_symbol("kucoin", "BCHABC-ETH")
   }
 
-  expect(symbol == symbol_cache).toBe(true)*/
+  expect(symbol == symbol_cache).toBe(true)
 })
