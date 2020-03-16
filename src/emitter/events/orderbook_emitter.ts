48aeda19c64fa0d8eed8ba407bd9d80c1e97437a
T <52665225+valamidev@users.noreply.github.com>
2020-03-16 15:35:50 +0100

    Fix memory leak at Orderbook emitter

--- src/emitter/events/orderbook_emitter.ts
+++ src/emitter/events/orderbook_emitter.ts
@@ -12,7 +12,7 @@ import { Redis, RedisPub } from '../../redis/redis';
 import { TableTemplates } from '../../database/queries/enums';
 
 const memoryLimit = 512;
-const Orderbooks = {};
+const OrderBookExchangeCache: Map<string, any> = new Map();
 
 interface OrderBookDepth {
   symbol: string;
@@ -27,69 +27,82 @@ class OrderbookEmitter {
     // Event listeners
     logger.verbose('Orderbook Emitter started!');
 
-    Emitter.on('Orderbook', (exchange: string, depth: OrderBookDepth) => {
-      exchange = exchange.toLowerCase();
+    Emitter.on(
+      'Orderbook',
+      async (exchange: string, depth: OrderBookDepth): Promise<void> => {
+        exchange = exchange.toLowerCase();
 
-      if (typeof Orderbooks[exchange] == 'undefined') {
-        Orderbooks[exchange] = new OrderBookStore(memoryLimit);
-      }
+        if (!OrderBookExchangeCache[exchange]) {
+          OrderBookExchangeCache[exchange] = new OrderBookStore(memoryLimit);
+        }
 
-      const { symbol, asks, bids } = depth;
+        const { symbol, asks, bids } = depth;
 
-      if (Orderbooks[exchange]._symbols.indexOf(symbol) == -1) {
-        setImmediate(async () => {
+        if (OrderBookExchangeCache[exchange]._symbols.indexOf(symbol) === -1) {
           try {
             const tableName = util.orderbookName(exchange, symbol);
             const orderbookSnapshot = await Redis.get(tableName);
+
             if (orderbookSnapshot !== null) {
               const parsedOrderbookSnapshot = JSON.parse(orderbookSnapshot);
 
               if (parsedOrderbookSnapshot?.ask && parsedOrderbookSnapshot?.bid) {
-                Orderbooks[exchange].updateOrderBook(symbol, parsedOrderbookSnapshot.ask, parsedOrderbookSnapshot.bid);
+                OrderBookExchangeCache[exchange].updateOrderBook(symbol, parsedOrderbookSnapshot.ask, parsedOrderbookSnapshot.bid);
               }
             }
-            Orderbooks[exchange].updateOrderBook(symbol, asks, bids);
+            OrderBookExchangeCache[exchange].updateOrderBook(symbol, asks, bids);
           } catch (e) {
-            logger.error('Orderbook snapshot error', e);
+            logger.error('Orderbook loading error', e);
           }
-        });
-      } else {
-        Orderbooks[exchange].updateOrderBook(symbol, asks, bids);
+        } else {
+          try {
+            OrderBookExchangeCache[exchange].updateOrderBook(symbol, asks, bids);
 
-        if (typeof Orderbooks[exchange]._data[symbol] != 'undefined') {
-          const data = Orderbooks[exchange]._data[symbol];
+            const data = { ...OrderBookExchangeCache[exchange]._data[symbol] };
 
-          RedisPub.publish('OrderBookUpdate', JSON.stringify({ exchange, symbol, ask: data.best_ask, bid: data.best_bid }));
-        }
-      }
-    });
-
-    Emitter.on('OrderbookSnapshot', (snapshotTime: number) => {
-      Object.keys(Orderbooks).forEach(exchange => {
-        const symbols = Orderbooks[exchange]._symbols;
-
-        symbols.forEach((symbol: string) => {
-          setImmediate(async () => {
-            const orderbook = Orderbooks[exchange].getOrderBook(symbol);
-            // Get CCXT standard symbol
-            const ccxtSymbol = await TradepairQueries.idToSymbol(exchange, symbol);
-
-            if (ccxtSymbol) {
-              const tableName = util.orderbookName(exchange, ccxtSymbol);
-
-              if (!(await DBQueries.tableCheck(tableName))) {
-                await DBQueries.createNewTableFromTemplate(TableTemplates.Orderbook, tableName);
-              }
-
-              await DBQueries.orderbookReplace(tableName, { time: snapshotTime, orderbook });
-
-              // Store snapshot in redis for 600 sec
-              Redis.set(tableName, JSON.stringify(orderbook), 'EX', 600);
+            if (data) {
+              await RedisPub.publish('OrderBookUpdate', JSON.stringify({ exchange, symbol, ask: data.best_ask, bid: data.best_bid }));
             }
-          });
-        });
-      });
-    });
+          } catch (e) {
+            logger.error('Orderbook update error', e);
+          }
+        }
+      },
+    );
+
+    Emitter.on(
+      'OrderbookSnapshot',
+      async (snapshotTime: number): Promise<void> => {
+        const exchanges = Object.keys(OrderBookExchangeCache);
+
+        for (const exchange of exchanges) {
+          const symbols = OrderBookExchangeCache[exchange]._symbols;
+
+          for (const symbol of symbols) {
+            try {
+              const orderbook = { ...OrderBookExchangeCache[exchange].getOrderBook(symbol) };
+              // Get CCXT standard symbol
+              const ccxtSymbol = await TradepairQueries.idToSymbol(exchange, symbol);
+
+              if (ccxtSymbol) {
+                const tableName = util.orderbookName(exchange, ccxtSymbol);
+
+                if (!(await DBQueries.tableCheck(tableName))) {
+                  await DBQueries.createNewTableFromTemplate(TableTemplates.Orderbook, tableName);
+                }
+
+                await DBQueries.orderbookReplace(tableName, { time: snapshotTime, orderbook });
+
+                // Store snapshot in redis for 600 sec
+                Redis.set(tableName, JSON.stringify(orderbook), 'EX', 600);
+              }
+            } catch (e) {
+              logger.error('Orderbook snapshot error', e);
+            }
+          }
+        }
+      },
+    );
   }
 }
 
